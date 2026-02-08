389f4952ede41ef76f8250d41b952c72f1eee0e4
Arthur Dos Santos Almeida <arthur.almeida@g.globo>
2026-02-08 16:31:11 -0300

    criando classe filme

---
+++ screenmatch/src/main/java/br/com/alura/screenmatch/model/Filme.java
@@ -0,0 +1,9 @@
+package br.com.alura.screenmatch.model;
+
+public class Filme {
+  private String titulo;
+  private int duracaoMinutos;
+  private boolean incluidoNoPlano;
+  private double notaMedia;
+  private String url;
+}
